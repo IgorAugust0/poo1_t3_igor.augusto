bb5ccc06645b983d1dead0bb777ed7f7aecf98e7
IgorAugust0 <79866605+IgorAugust0@users.noreply.github.com>
2022-10-03 20:12:49 -0300

    ex01 test

---
+++ LAB01 - Procedural/ex01/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+        System.out.println("Nome: Igor Augusto Reis Gomes");
+        System.out.println("Matrícula: 12011BSI290");
+    }
+}
