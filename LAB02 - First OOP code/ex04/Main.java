0ab3cc2eed216d2e696d79c3d0985eb0569b1f19
IgorAugust0 <augustoigor269@gmail.com>
2022-11-22 15:50:10 -0300

    update

--- LAB02 - First OOP code/ex04/Main.java
+++ LAB02 - First OOP code/ex04/Main.java
@@ -8,7 +8,6 @@ public class Main {
         jogador[1] = new Figurinha("Ronaldo", LocalDate.parse("1985-02-05"), 1.85f, 80.5f, "Atacante", "Brasil");
         jogador[2] = new Figurinha("Neymar", LocalDate.parse("1992-02-05"), 1.75f, 68.5f, "Atacante", "Brasil");
         
-
         Selecao selecao = new Selecao("Brasil", "Tite", "Brasil", 1914, 5);
         for (int i = 0; i < jogador.length; i++) {
             selecao.setJogador(jogador[i], i);
@@ -19,14 +18,5 @@ public class Main {
             jogador[i].imprimir();
         }
 
-        /*
-        Figurinha j1 = new Figurinha("Messi", LocalDate.parse("1990-10-23"), 1.7f, 72.5f, "Atacante", "Argentina");
-        Figurinha j2 = new Figurinha("Ronaldo", LocalDate.parse("1985-02-05"), 1.85f, 80.5f, "Atacante", "Brasil");
-        Figurinha j3 = new Figurinha("Neymar", LocalDate.parse("1992-02-05"), 1.75f, 68.5f, "Atacante", "Brasil");
-        
-        j1.imprimir();
-        j2.imprimir();
-        j3.imprimir();
-        */
     }
 }
--- LAB02 - First OOP code/ex05/Main.java
+++ LAB02 - First OOP code/ex05/Main.java
@@ -17,15 +17,5 @@ public class Main {
         for (int i = 0; i < jogador.length; i++) {
             jogador[i].imprimir();
         }
-
-        /*
-        Figurinha j1 = new Figurinha("Messi", LocalDate.parse("1990-10-23"), 1.7f, 72.5f, "Atacante", "Argentina");
-        Figurinha j2 = new Figurinha("Ronaldo", LocalDate.parse("1985-02-05"), 1.85f, 80.5f, "Atacante", "Brasil");
-        Figurinha j3 = new Figurinha("Neymar", LocalDate.parse("1992-02-05"), 1.75f, 68.5f, "Atacante", "Brasil");
-        
-        j1.imprimir();
-        j2.imprimir();
-        j3.imprimir();
-        */
     }
 }
