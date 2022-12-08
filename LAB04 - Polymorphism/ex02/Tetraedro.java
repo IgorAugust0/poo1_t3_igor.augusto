88e789c070413d08f88ce91537e118e167b62e88
IgorAugust0 <augustoigor269@gmail.com>
2022-12-08 14:44:39 -0300

    Update Tetraedro.java

--- LAB04 - Polymorphism/ex02/Tetraedro.java
+++ LAB04 - Polymorphism/ex02/Tetraedro.java
@@ -7,14 +7,17 @@ public class Tetraedro extends FormaTridimensional {
         this.lado = lado;
     }
 
+    @Override
     public double ObterArea() {
         return Math.pow(this.lado, 2) * Math.sqrt(3);
     }
 
+    @Override
     public double ObterVolume(){
         return (Math.pow(this.lado, 3) * Math.sqrt(2)) / 12;
     }
 
+    @Override
     public String descricao(){
         return "É um tetraedro";
     }
